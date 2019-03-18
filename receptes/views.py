0f7c5342926bef552d4658bb4cc7df449aec86cd
Carme <carme@almarsa.es>
2019-03-18 17:45:26 +0100

    La plantilla agafa info de la bbdd

--- receptes/views.py
+++ receptes/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Recepta
 
 # Mostra un llistat amb totes les receptes.
 def llista_receptes(request):
-    return render(request, 'receptes/llista_receptes.html', {})
+    receptes = Recepta.objects.filter(actiu=True).order_by('data_creacio')
+    return render(request, 'receptes/llista_receptes.html', {'receptes':receptes})
